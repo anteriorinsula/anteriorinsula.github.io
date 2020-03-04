48386c3d75af9323c94bda09f5a97225a5907c45
joachimbovin <joachimbovin@hotmail.com>
2020-03-04 23:04:17 +0100

    First push website spatiebalk

---
+++ function.js
@@ -0,0 +1,38 @@
+
+
+function toggle_image(id, image_src1, image_src2) {
+
+    var image = document.getElementById(id);
+    var parts = image.src.split("/");
+    var image_src = parts[parts.length-1];
+    //console.log(image.src)
+    if (image_src=== image_src1){
+
+        image.src= "images/social/" + image_src2;
+    }
+    else if (image_src=== image_src2){
+
+        image.src= "images/social/" + image_src1;
+    }
+    else {
+        image.src = "images/social/" + image_src1;
+    }
+}
+
+
+$(document).ready(function() {
+
+
+    $('body').keyup(function(e){
+
+        if(e.keyCode == 32){
+            // user has pressed space
+            $('#easter_egg').append("♥");
+        }
+    });
+
+
+
+
+
+});
